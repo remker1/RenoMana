0df91030b2cc21f45a79ea4185030ceb14864302
Dann <dgp867@mail.usask.ca>
2023-09-28 15:16:56 -0600

    tinkering with fx files, trying to use cmpt381 lab hand in as template

--- JavaFx subgroup/LoginPageDannPangan/LogInPage.java
+++ JavaFx subgroup/LoginPageDannPangan/LogInPage.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
