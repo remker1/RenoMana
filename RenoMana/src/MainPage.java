30d0f802c8fcf5172c3166fadab94324dfffd831
Jewel Magcawas <jewelmagcawas@Jewels-MBP.hitronhub.home>
2023-10-01 06:42:38 -0600

    (Jewel) Initial commit before starting MainPage(Homepage) implementation.

--- RenoMana/src/MainPage.java
+++ RenoMana/src/MainPage.java
@@ -1,3 +1,17 @@
+/**
+ * MainPage Class
+ * <p>
+ * The application has a user interface with several tab where each tab shows a different feature
+ * or part of the application. By putting the content of each tab into its own class, the
+ * application makes it easier to change and maintain. The content of each tab is made and
+ * put into place in its own class. This method lets developers work on single features at a time,
+ * which makes the codebase more organised and easier to use.
+ *
+ * @author Jewel Magcawas
+ * @version 1.0
+ * @since 2023-08-01
+ */
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -9,6 +23,5 @@ public class MainPage extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
     }
 }
