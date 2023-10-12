7a222d1d4bee039d58b1380bc7e783de065b61d1
Dann <dgp867@mail.usask.ca>
2023-10-11 19:33:53 -0600

    Scheduler Skeleton - name, timeline, details, members

--- RenoMana/src/Scheduler.java
+++ RenoMana/src/Scheduler.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
@@ -8,19 +9,35 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
+public class Scheduler extends Application {
 
-public class Scheduler {
+    private TableView table = new TableView();
+
     public void start(Stage stage) {
+        Scene scene = new Scene(new Group());
+        stage.setTitle("Scheduler");
+        stage.setWidth(360);
+        stage.setHeight(500);
 
+        final Label label = new Label("Projects Schedule");
+        label.setFont(new Font("Arial", 20));
 
+        table.setEditable(true);
 
-        VBox root = new VBox(10);
-        root.setPadding(new Insets(20));
-        root.getChildren().addAll();
+        TableColumn projName = new TableColumn("Name");
+        TableColumn projTimeline = new TableColumn("Timeline");
+        TableColumn projDetails = new TableColumn("Details");
+        TableColumn projMembers = new TableColumn("Members");
 
-        Scene scene = new Scene(root, 500, 800);
-        stage.setTitle("Log In");
+        table.getColumns().addAll(projName, projTimeline, projDetails, projMembers);
+
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
         stage.setScene(scene);
         stage.show();
     }
