2a5397f94fa8d4cf75ca49d26d095c94013813df
jewelmagcawas <wzt296@usask.ca>
2023-10-13 18:02:03 -0600

    Implemented the add a new item method to the table with invalid input error handling. Tested the method by typing wrong data types.

--- RenoMana/src/inventoryMana/Inventory.java
+++ RenoMana/src/inventoryMana/Inventory.java
@@ -1,5 +1,7 @@
 package inventoryMana;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
@@ -13,11 +15,12 @@ public class Inventory extends VBox {
     private ObservableList<InventoryItem> data;
 
      public Inventory(){
-         // Setting up the table.
+         // Setting up the table
          inventoryTable = new TableView<>();
+         inventoryTable.prefWidthProperty().bind(this.widthProperty());
          data = FXCollections.observableArrayList();
 
-         // Adding column names.
+         // Adding column names
          TableColumn<InventoryItem, String> toolNameCol = new TableColumn<>("Tool Name");
          toolNameCol.setCellValueFactory(cellData -> cellData.getValue().toolNameProperty());
          toolNameCol.prefWidthProperty().bind(inventoryTable.widthProperty().multiply(0.3)); // 40% width
@@ -37,12 +40,12 @@ public class Inventory extends VBox {
          inventoryTable.setItems(data);
 
 
-         // Setting up the button options for things user can do in this tab.
+         // Setting up the button options for things user can do in this tab
          Button addItem = new Button("Add");
          addItem.setOnAction(actionEvent -> addInventoryItem());
 
          Button deleteItem = new Button("Delete");
-         deleteItem.setOnAction(actionEvent -> deleteInvetoryItem());
+         deleteItem.setOnAction(actionEvent -> deleteInventoryItem());
 
          Button modifyItem = new Button("Modify");
          modifyItem.setOnAction(actionEvent -> {modifyInventoryItem();});
@@ -71,10 +74,64 @@ public class Inventory extends VBox {
     private void modifyInventoryItem() {
     }
 
-    private void deleteInvetoryItem() {
+    private void deleteInventoryItem() {
     }
 
     private void addInventoryItem() {
+         // Gather user input for tool, quantity, and estimation values
+        TextInputDialog toolInput = new TextInputDialog();
+        toolInput.setTitle("Add New Tool");
+        toolInput.setHeaderText("Enter Tool Name");
+        String toolName = toolInput.showAndWait().orElse("");
+
+        // Error Handling: If tool name is already in the table
+        for (InventoryItem item : this.data) {
+            if (item.getToolName().equals(toolName)){
+                Alert duplicateAlert = new Alert(Alert.AlertType.ERROR);
+                duplicateAlert.setTitle("Error!");
+                duplicateAlert.setHeaderText("Tool already exists!");
+                duplicateAlert.setContentText("Please use the modify button to change the existing tool's " +
+                        "details instead!");
+                duplicateAlert.showAndWait();
+                return;
+            }
+        }
+
+        int quantity = 0;
+        int estimation = 0;
+
+        try {
+            TextInputDialog quantityInput = new TextInputDialog("0");
+            quantityInput.setHeaderText("Enter Quality");
+            quantity = Integer.parseInt(quantityInput.showAndWait().orElse("Invalid Input!"));
+
+            if (quantity < 0) {
+                throw new NumberFormatException();
+            }
+
+            TextInputDialog estimateInput = new TextInputDialog("0");
+            estimateInput.setHeaderText("Enter Estimate");
+            estimation = Integer.parseInt(estimateInput.showAndWait().orElse("Invalid Input!"));
+
+            if (estimation < 0) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            Alert invalidNum = new Alert(Alert.AlertType.ERROR);
+            invalidNum.setTitle("Error!");
+            invalidNum.setHeaderText("Invalid Input!");
+            invalidNum.setContentText("Please enter a valid positive integer for quantity and estimation.");
+            invalidNum.showAndWait();
+            return;
+        }
+
+        // Create the InventoryItem
+        InventoryItem newItem = new InventoryItem(new SimpleStringProperty(toolName),
+                new SimpleIntegerProperty(quantity), new SimpleIntegerProperty(estimation));
+
+        this.data.add(newItem);
+        inventoryTable.refresh();
+
     }
 
 }
