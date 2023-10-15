022d66e2fcf95c0f04602b9bbfd1126226a09152
Dann <dgp867@mail.usask.ca>
2023-10-15 17:20:16 -0600

    calendar and scheduler added comments to each

--- RenoMana/src/Calendar.java
+++ RenoMana/src/Calendar.java
@@ -8,34 +8,42 @@ import javafx.stage.Stage;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Calendar extends Application {
 
-    private BorderPane rootLayout;
+    // main layout
+    private BorderPane root;
     LocalDate today = LocalDate.now();
 
 
     @Override
     public void start(Stage primaryStage) {
-        rootLayout = new BorderPane();
-        Scene scene = new Scene(rootLayout, 800, 600);
 
+        root = new BorderPane();
+        Scene scene = new Scene(root, 900, 800);
+
+        // dropdown list for selecting calendar view
         ComboBox<String> viewSelector = new ComboBox<>();
         viewSelector.getItems().addAll("Day", "Week", "Month", "Year");
-        viewSelector.setValue("Month");
+        viewSelector.setValue("Month"); // default drop down view option will be month
         viewSelector.setOnAction(event -> switchView(viewSelector.getValue()));
 
-        rootLayout.setTop(viewSelector);
+        // add dropdown list to the top of the main layout
+        root.setTop(viewSelector);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Calendar App");
         primaryStage.show();
 
+        // set view to month when application starts
         switchView("Month");
     }
 
+    /***
+     * Method to switch calendar display based on selected view
+     * @param view either day, week, month, or year
+     */
     private void switchView(String view) {
         switch (view) {
             case "Day":
@@ -53,10 +61,13 @@ public class Calendar extends Application {
         }
     }
 
+    /***
+     * Method to set up the calendar for monthly view
+     */
     private void setupMonthView() {
         GridPane monthGrid = new GridPane();
 
-        // current month
+        // current month and its first day
         LocalDate currentDate = LocalDate.now();
         LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
         DayOfWeek firstDayOfWeek = firstDayOfMonth.getDayOfWeek();
@@ -64,25 +75,31 @@ public class Calendar extends Application {
         // calculate the starting cell for the 1st of the month
         int startCell = firstDayOfWeek.getValue() % 7;
 
+        // variables to keep track of current day and date being set up
         int dayOfMonth = 1;
         LocalDate dateBeingSetup = firstDayOfMonth;
 
+        // populate the calendar grid for the month
         for (int i = 0; i < 6; i++) { // for up to 6 weeks in a month
             for (int j = startCell; j < 7; j++) { // 7 days in a week
 
+                // stop once all days of the month are added
                 if (dayOfMonth > firstDayOfMonth.lengthOfMonth()) {
                     break;
                 }
 
+                // button for each day
                 Button dayButton = new Button(String.valueOf(dayOfMonth));
                 dayButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
-                int finalDayOfMonth = dayOfMonth;
+                // temporary function: display the date when button is clicked
                 LocalDate finalDateBeingSetup = dateBeingSetup;
                 dayButton.setOnAction(e -> {
                     System.out.println("Clicked on: " + finalDateBeingSetup.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                    // other functionalities
                 });
 
+                // add button to calendar grid
                 monthGrid.add(dayButton, j, i);
                 dayOfMonth++;
                 dateBeingSetup = dateBeingSetup.plusDays(1);
@@ -90,50 +107,71 @@ public class Calendar extends Application {
             startCell = 0; // reset start cell for the next row/week
         }
 
-        rootLayout.setCenter(monthGrid);
+        // update main layout to display the month grid
+        root.setCenter(monthGrid);
     }
 
+    /***
+     * Sets up the calendar for weekly view
+     */
     private void setupWeekView() {
         GridPane weekGrid = new GridPane();
 
-        // current week
+        // start of the current week
         LocalDate currentDate = LocalDate.now();
-        LocalDate startOfWeek = currentDate.minusDays(currentDate.getDayOfWeek().getValue());
+        // to make sure the week starts on sunday
+        int daysToSubtract = currentDate.getDayOfWeek() == DayOfWeek.SUNDAY ? 0 : currentDate.getDayOfWeek().getValue();
+        LocalDate startOfWeek = currentDate.minusDays(daysToSubtract);
 
+        // populate the calendar grid for the week
         for (int i = 0; i < 7; i++) { // 7 days in a week
             LocalDate dayInWeek = startOfWeek.plusDays(i);
 
+            // create a button for each day with number and day of the week
             Button dayButton = new Button(dayInWeek.getDayOfMonth() + "\n" + dayInWeek.format(DateTimeFormatter.ofPattern("EEE")));
             dayButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
+            // temporary functionality: display the date when button is clicked
             dayButton.setOnAction(e -> {
                 System.out.println("Clicked on: " + dayInWeek.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                // other functionalities
             });
 
+            // add button to the grid
             weekGrid.add(dayButton, i, 0);
         }
 
-        rootLayout.setCenter(weekGrid);
+        // update the main layout to display the week grid
+        root.setCenter(weekGrid);
     }
 
+    /***
+     * Sets up the calendar for yearly view
+     */
     private void setupYearView() {
         GridPane yearGrid = new GridPane();
 
         LocalDate currentDate = LocalDate.now().withDayOfYear(1); // start of the year
 
+        // populate the calendar grid for the year
         for (int month = 0; month < 12; month++) {
             GridPane monthGrid = new GridPane();
             int daysInMonth = currentDate.lengthOfMonth();
             int dayCount = 1;
 
-            for (int week = 0; week < 6; week++) { // max 6 rows for weeks
+            // iterate through a potential maximum of 6 weeks for each month
+            for (int week = 0; week < 6; week++) {
                 for (int day = 0; day < 7 && dayCount <= daysInMonth; day++) {
                     LocalDate dayDate = currentDate.withDayOfMonth(dayCount);
 
+                    // check if the day matches with the week day
                     if (dayDate.getDayOfWeek().getValue() % 7 == day) { //
+                        // add to the month grid if it matches
                         Button dayButton = new Button(String.valueOf(dayCount));
+                        // temporary functionality: display date clicked
                         dayButton.setOnAction(e -> {
                             System.out.println("Clicked on: " + dayDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                            // other functionalities
                         });
 
                         monthGrid.add(dayButton, day, week);
@@ -142,26 +180,36 @@ public class Calendar extends Application {
                 }
             }
 
-            yearGrid.add(monthGrid, month % 4, month / 4); // 4x3 layout
+            // add each month grid to te year grid
+            yearGrid.add(monthGrid, month % 4, month / 4); // 4 months per row layout
 
+            // move to the next month
             currentDate = currentDate.plusMonths(1);
         }
 
-        rootLayout.setCenter(yearGrid);
+        // update the main layout to display the year grid
+        root.setCenter(yearGrid);
     }
 
+    // helper function to create a button representing an hour in the day view
     private Button createHourButton(int hour) {
         String hourText = String.format("%02d:00 - %02d:59", hour, hour);
         Button hourButton = new Button(hourText);
         hourButton.setMaxWidth(Double.MAX_VALUE);  // to take full width
 
+        // temporary functionality: display the selected hour when button is clicked
         hourButton.setOnAction(e -> {
             System.out.println("Selected hour: " + hour);
+            // other functionalities
         });
 
         return hourButton;
     }
 
+    /***
+     * Sets up the calendar for daily view
+     * @param selectedDate whichever day is being viewed
+     */
     private void setupDayView(LocalDate selectedDate) {
         VBox dayVBox = new VBox(5); // 5 is the spacing between items
 
@@ -169,11 +217,13 @@ public class Calendar extends Application {
         Label dateLabel = new Label(selectedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd, EEEE")));
         dayVBox.getChildren().add(dateLabel);
 
+        // create buttons for each hour of the day and add them to the day VBox
         for (int hour = 0; hour < 24; hour++) {
             dayVBox.getChildren().add(createHourButton(hour));
         }
 
-        rootLayout.setCenter(dayVBox);
+        // update the main layout to display the day view
+        root.setCenter(dayVBox);
     }
 
 
--- RenoMana/src/Scheduler.java
+++ RenoMana/src/Scheduler.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 
 public class Scheduler extends Application {
 
+    // to display schedule in a table format
     private TableView table = new TableView();
 
     public void start(Stage stage) {
@@ -18,23 +19,47 @@ public class Scheduler extends Application {
         stage.setWidth(780);
         stage.setHeight(500);
 
+        // Project Schedule label
         final Label label = new Label("Projects Schedule");
         label.setFont(new Font("Arial", 20));
 
+        // make table editable
         table.setEditable(true);
 
+        // columns with appropriate headers
         TableColumn projName = new TableColumn("Name");
         TableColumn projTimeline = new TableColumn("Timeline");
-        projTimeline.setPrefWidth(500);
+        projTimeline.setPrefWidth(500); // timeline will contain colour coded blocks for each project phase
         TableColumn projDetails = new TableColumn("Details");
+        TableColumn projMembers = getTableColumn();
+
+        // add all defined columns to TableView
+        table.getColumns().addAll(projName, projTimeline, projDetails, projMembers);
+
+        // VBox to organize each project vertically
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label, table);
+
+        // add VBox to the root of the scene
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private static TableColumn getTableColumn() {
         TableColumn projMembers = new TableColumn("Members");
 
+        // dropdown view for project members column
         projMembers.setCellFactory(param -> new TableCell() {
             final ComboBox<String> comboBox = new ComboBox<>(FXCollections.observableArrayList("Member A", "Member B", "Member C"));
 
             @Override
             protected void updateItem(Object item, boolean empty) {
                 super.updateItem(item, empty);
+                // display dropdown menu if cell is not empty
                 if (!empty) {
                     setGraphic(comboBox);
                 } else {
@@ -42,18 +67,7 @@ public class Scheduler extends Application {
                 }
             }
         });
-
-        table.getColumns().addAll(projName, projTimeline, projDetails, projMembers);
-
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        vbox.getChildren().addAll(label, table);
-
-        ((Group) scene.getRoot()).getChildren().addAll(vbox);
-
-        stage.setScene(scene);
-        stage.show();
+        return projMembers;
     }
 
     public static void main(String[] args) {
