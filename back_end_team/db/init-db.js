0a6655f611e073e3ed7a85cc6e26f125fcc4d1d2
Emmanuel <eka722@usask.ca>
2023-10-19 21:25:58 -0600

    Establish object ID references in projects collection
    In this commit:
    - Added references to inventory items using `ObjectId` values
    - Associated inventory items with respective projects

--- back_end_team/db/init-db.js
+++ back_end_team/db/init-db.js
@@ -35,28 +35,28 @@ db.employees.insertMany([
 // Creation of the "inventory" collection
 db.inventory.insertMany([
     {
-        "id": 101,
+        "id": ObjectId("101"),
         "name": "Wrench_1",
         "serial_number": "ABC12345",
         "manufacturer": "Craftsman",
         "warranty": "2023/10/19"
     },
     {
-        "id": 102,
+        "id": ObjectId("102"),
         "name": "Wrench_2",
         "serial_number": "XYZ98765",
         "manufacturer": "Snap-on",
         "warranty": "2023/11/19"
     },
     {
-        "id": 103,
+        "id": ObjectId("103"),
         "name": "Screwdriver_1",
         "serial_number": "PQR56789",
         "manufacturer": "Stanley",
         "warranty": "2023/06/19"
     },
     {
-        "id": 103,
+        "id": ObjectId("104"),
         "name": "PipeBender_1",
         "serial_number": "PQR56789",
         "manufacturer": "IRWIN Tools",
@@ -69,9 +69,10 @@ db.projects.insertMany([
     {
         "id": 201,
         "name": "Smith bathroom renovation",
-        "client": "Henry Smith",
+        "client": "Henry John Smith",
         "end_date": "2023-12-31",
         "location": "123 Main Street"
+        "inventory_item(s)": [ObjectId(101)]
     },
     {
         "id": 202,
@@ -79,6 +80,7 @@ db.projects.insertMany([
         "client": "ABC Corporation",
         "end_date": "2024-02-15",
         "location": "456 Business Avenue"
+        "inventory_item(s)": [ObjectId(102)]
     },
     {
         "id": 203,
@@ -86,5 +88,14 @@ db.projects.insertMany([
         "client": "Green Thumb Gardeners",
         "end_date": "2023-10-15",
         "location": "789 Garden Road"
+        "inventory_item(s)": [ObjectId(103)]
+    },
+    {
+       "id": 204,
+       "name": "Office Furnishing",
+       "client": "Tech Solutions Inc.",
+       "end_date": "2023-11-30",
+       "location": "456 Business Avenue",
+       "inventory_item(s)": [ObjectId("item_003"), ObjectId("item_004")]
     }
 ]);
