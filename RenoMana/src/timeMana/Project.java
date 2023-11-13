f177e0b0e47f3d95331f9c076fd59409bb135de1
Dann <dgp867@mail.usask.ca>
2023-11-13 16:17:42 -0600

    added colour visualization to scheduler timeline

--- RenoMana/src/timeMana/Project.java
+++ RenoMana/src/timeMana/Project.java
@@ -4,6 +4,10 @@ import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class Project {
     private final SimpleStringProperty name;
     private final SimpleStringProperty timeline;
@@ -63,5 +67,31 @@ public class Project {
 
     public void setMembers(ObservableList<String> members) {
         this.members.set(members);
+    }
+
+    public String getTimelineStatus() {
+        try {
+            // parse timeline string into LocalDate object
+            LocalDate dueDate = LocalDate.parse(timeline.get(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            // current date to compare due date with
+            LocalDate currentDate = LocalDate.now();
+
+            // calculate the number of days until the project is due
+            long daysUntilDue = ChronoUnit.DAYS.between(currentDate, dueDate);
+
+            // determine status based on days until due date
+            if (daysUntilDue < 0) {
+                return "overdue"; // when project is past its due date
+            } else if (daysUntilDue < 7) {
+                return "red"; // due in less than a week
+            } else if (daysUntilDue <= 30) {
+                return "yellow"; // due in more than a week but less than a month
+            } else {
+                return "green"; // due in more than a month
+            }
+        } catch (Exception e) {
+            // parsing exceptions
+            return "unknown"; // could not determine the status
+        }
     }
 }
