f078c9c74f9ac890d7482a9778aa9159f33128c2
jewelmagcawas <wzt296@usask.ca>
2023-11-21 18:32:21 -0600

    Deleted empty classes

--- RenoMana/src/MainPageTab4.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.layout.VBox;
-import static javafx.application.Application.launch;
-
-public class MainPageTab4 extends VBox {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
