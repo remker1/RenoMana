07359f093ba915ffae8152d849df81cc84b4751e
Emmanuel <eka722@usask.ca>
2023-11-29 01:10:42 -0600

    Create inquiryMana and ProjectItems components  (2nd time. Commit didn't go through)
    - Added inquiryMana package with classes for handling inquiries.
    - Implemented ProjectItems component for managing project items in the UI.
    
    Modify backend for new endpoints and database extraction
    
    - Updated backend to handle new endpoints for inquiries and projects.
    - Enhanced database queries for extracting data related to inquiries and projects.
    - Ensure proper communication between frontend and backend for new features (tables and new tabs)

--- back_end_team/app/app.py
+++ back_end_team/app/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 client = MongoClient(host='db', port=27017, username='root', password='pass')
 db = client.renoGp
 
+
 # Displays the main Reno Group landing page
 @app.route('/', methods=['GET'])
 def displayLanding():
@@ -14,6 +15,7 @@ def displayLanding():
         return render_template('landing.html')
     except:
         pass
+
 
 # Displays the RenoGrpRequestPage.html webpage and allows user to input data to mango_tb database table
 @app.route('/requestPage', methods=['GET'])
@@ -23,6 +25,7 @@ def displayRequestPage():
     except:
         pass
 
+
 # Displays the RenoGrpReviewPage.html webpage and allows users to add and view customer reviews
 @app.route('/reviews', methods=['GET'])
 def displayReviews():
@@ -31,12 +34,14 @@ def displayReviews():
     except:
         pass
 
+
 @app.route('/inquiry', methods=['GET'])
 def displayInquiry():
     try:
         return render_template('RenoGrpInquiryPage.html')
     except:
         pass
+
 
 # Initialize some default employees into the database
 @app.route('/initDB', methods=["GET"])
@@ -67,7 +72,6 @@ def initDB():
             }
         ]
         result = db['employees'].insert_many(employeeList)
-        
 
         response = {
             'status': 'success',
@@ -84,6 +88,7 @@ def initDB():
             'message': str(e)
         }
         return jsonify(response), 500
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -117,8 +122,6 @@ def register():
         result1 = db['auth'].insert_one(auth_document)
         result2 = db['employees'].insert_one(employee_document)
 
-
-
         response = {
             'status': 'success',
             'message': 'Document added successfully',
@@ -132,11 +135,12 @@ def register():
         print(f'Error in register route: {e}')
         return jsonify({"status": "failure", "message": str(e)}), 500
 
+
 # Route for submitting login
 @app.route('/login', methods=['POST'])
 def login():
     try:
-        data = request.get_json() #{username: ____, password:______}
+        data = request.get_json()  # {username: ____, password:______}
         print(data)
 
         input_user = data['username']
@@ -178,6 +182,7 @@ def login():
         print(f'Error in register route: {e}')
         return jsonify({"status": "failure", "message": str(e)}), 500
 
+
 @app.route('/getDashboardData', methods=['POST'])
 def getDashboardData():
     try:
@@ -204,17 +209,18 @@ def getDashboardData():
             "message": str(e)
         }
         return jsonify(response), 500
-    
+
+
 @app.route('/getProjectsData', methods=['POST'])
 def getProjectsData():
     try:
         data = request.get_json()
         cookie = data['username']
         print("cookie: " + cookie)
-        
+
         # Fetching the documents from MongoDB
         cursor = db['projects'].find({})
-        
+
         # Converting cursor to a list and then to JSON
         response = json_util.dumps(list(cursor))
         json_data = json_util.dumps(list(cursor))
@@ -233,11 +239,23 @@ def getProjectsData():
         }
         return jsonify(response), 500
 
+
 # Route for submitting requests
 @app.route('/submitRequest', methods=['GET', 'POST'])
 def submit_request():
     try:
-        if request.method == 'POST':
+        if request.method == 'GET':
+            # Retrieve data from MongoDB collection
+            user_data = db['user'].find({}, {'_id': 0, 'customerName': 1, 'customerEmail': 1, 'customerCell': 1,
+                                             'company': 1, 'startDate': 1, 'endDate': 1, 'projectDesc': 1})
+
+            # Convert cursor to list for JSON serialization
+            user_data_list = list(user_data)
+
+            # Return data as JSON response
+            return jsonify({"data": user_data_list})
+
+        elif request.method == 'POST':
             # Extract user inputs from the HTML form
             # data_1000 = request.get_json()
             customerName = request.form.get("customerName")
@@ -247,7 +265,6 @@ def submit_request():
             startDate = request.form.get("startDate")
             endDate = request.form.get("endDate")
             projectDesc = request.form.get("projectDesc")
-            projectInq = request.form.get("projectInq")
 
             user_document = {
                 'customerName': customerName,
@@ -259,12 +276,8 @@ def submit_request():
                 'projectDesc': projectDesc
             }
 
-            inq_document = {
-                'projectInq': projectInq
-            }
             # Insert the user data into the MongoDB collection
             result100 = db['user'].insert_one(user_document)
-            result200 = db['inq'].insert_one(inq_document)
 
             response_1000 = {
                 'status': 'success',
@@ -282,17 +295,33 @@ def submit_request():
         }
         return jsonify(response_1000), 500
 
+
 @app.route('/submitinquiry', methods=['GET', 'POST'])
 def submit_inquiry():
     try:
-        if request.method == 'POST':
+        if request.method == 'GET':
+            # Retrieve data from MongoDB collection
+            user_data = db['inq'].find({}, {'_id': 0, 'Name': 1, 'Contact': 1, 'projectInq': 1})
+
+            # Convert cursor to list for JSON serialization
+            user_data_list = list(user_data)
+
+            # Return data as JSON response
+            return jsonify({"inq": user_data_list})
+
+        elif request.method == 'POST':
             # Extract user inputs from the HTML form
             # data_1000 = request.get_json()
+            Name = request.form.get("Name")
+            Contact = request.form.get("Contact")
             projectInq = request.form.get("projectInq")
 
             inq_document = {
+                'Name': Name,
+                'Contact': Contact,
                 'projectInq': projectInq
             }
+
             # Insert the user data into the MongoDB collection
             result200 = db['inq'].insert_one(inq_document)
 
@@ -305,12 +334,13 @@ def submit_inquiry():
 
     except Exception as e:
         # Log the exception for debugging
-        print(f'Error in submit_request route: {e}')
+        print(f'Error in submit_inquiry route: {e}')
         response_1000 = {
             'status': 'failure',
             'message': str(e)
         }
         return jsonify(response_1000), 500
+
 
 # Route for adding reviews
 @app.route('/addReview', methods=['POST'])
@@ -318,9 +348,10 @@ def addReview():
     try:
         # Gather review information
         data = request.json
-    
+
         # Insert the review into the MongoDB collection
-        result = db['reviews'].insert_one({'title': data.get("title"), 'description': data.get("description"), 'rating': data.get("rating")})
+        result = db['reviews'].insert_one(
+            {'title': data.get("title"), 'description': data.get("description"), 'rating': data.get("rating")})
 
         response = {
             'status': 'success',
@@ -338,6 +369,7 @@ def addReview():
         }
         return jsonify(response), 500
 
+
 @app.route('/getReviews', methods=['POST'])
 def getReviews():
     try:
@@ -346,10 +378,11 @@ def getReviews():
         if (data.get("rating") == "0"):
             result = list(db['reviews'].find({}, {"title": 1, "description": 1, "rating": 1, "_id": 0}))
         else:
-            result = list(db['reviews'].find({"rating": data.get("rating")}, {"title": 1, "description": 1, "rating": 1 , "_id": 0}))
+            result = list(db['reviews'].find({"rating": data.get("rating")},
+                                             {"title": 1, "description": 1, "rating": 1, "_id": 0}))
         response = {
             "reviews": result
-            }
+        }
         return jsonify(response), 200
 
     except Exception as e:
@@ -361,13 +394,14 @@ def getReviews():
         }
         return jsonify(response), 500
 
+
 @app.route('/getEmployeeData', methods=['POST'])
 def getEmployeeData():
     try:
         data = request.get_json()
         queryUser = data['cookie']
 
-        cursor = db['employees'].find({"username":queryUser})
+        cursor = db['employees'].find({"username": queryUser})
         result = [doc for doc in cursor]
 
         for doc in result:
@@ -387,6 +421,7 @@ def getEmployeeData():
             "message": str(e)
         }
         return jsonify(response), 500
+
 
 # @app.route('/deleteEmployeeData', methods=['POST'])
 # def deleteEmployeeData():
@@ -432,6 +467,7 @@ def getProjects():
         }
         return jsonify(response), 500
 
+
 @app.route('/syncInventoryDelete', methods=['POST'])
 def syncInventoryDelete():
     try:
@@ -439,7 +475,7 @@ def syncInventoryDelete():
         deleteID = data['deleteItemID']
         print(deleteID)
 
-        result = db['inventory'].delete_one({"itemID":"\""+deleteID+"\""})
+        result = db['inventory'].delete_one({"itemID": "\"" + deleteID + "\""})
         print(result)
 
         if result:
