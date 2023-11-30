a8f1fece946cb31d6187b2ebba229124511d653a
remker1 <60105217+remker1@users.noreply.github.com>
2023-11-30 14:14:30 -0600

    get projects end point setup

--- RenoMana/src/timeMana/Projects.java
+++ RenoMana/src/timeMana/Projects.java
@@ -1,20 +1,4 @@
 package timeMana;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
 public class Projects {
-    private ObservableList<Project> projects;
-
-    public Projects() {
-        this.projects = FXCollections.observableArrayList();
-    }
-
-    public ObservableList<Project> getProjects() {
-        return projects;
-    }
-
-    public void setProjects(ObservableList<Project> projects) {
-        this.projects = projects;
-    }
 }
